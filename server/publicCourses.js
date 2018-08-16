2a16c0726246a4ce47908f85bc798f082fe3f42e
Carlos Saito <c.saito.style@gmail.com>
2018-08-16 15:41:00 +0200

    Fix. Remove "console.log"

--- server/publicCourses.js
+++ server/publicCourses.js
@@ -6,7 +6,6 @@ module.exports = async function () {
     uri: `${process.env.LMS_API_ROOT}/courses`,
     json: true
   })
-  console.log(Object.values(courses).filter(c => c.is_public).length)
 
   return Object.values(courses)
     .filter(c => c.is_public)
