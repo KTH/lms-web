d8f71dbabea5229ccd372a8bdb14208f907c7ae0
Carlos Saito <c.saito.style@gmail.com>
2018-08-16 10:12:51 +0200

    Basic Handlebars template

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const server = require('kth-node-server')
 const bunyan = require('bunyan')
+const exphbs  = require('express-handlebars');
+
 const logger = bunyan.createLogger({
   name: 'lms-export-logger',
   app: require('./package.json').name
 })
 
+server.engine('handlebars', exphbs({defaultLayout: 'main'}))
+server.set('view engine', 'handlebars')
+
 // If you need more routes, replace the following line with a separate Express
 // Router object
 server.get(process.env.PROXY_PREFIX_PATH || '', (req, res) => {
-  res.send('Hello World')
+  res.render('home')
 })
 
 server.start({
