c884a16ec750035818eedcc0f020f88aa71e2bb0
Carlos Saito <exacs@users.noreply.github.com>
2019-01-14 11:15:51 +0100

    Add hard-coded faked data

--- server/publicCourses.js
+++ server/publicCourses.js
@@ -11,8 +11,18 @@ let cache
 
 async function fetchCourses() {
   if (process.env.NODE_ENV === 'development' && process.env.USE_FAKE === 'true') {
-    return []
+    return [
+      {id: 3, sis_course_id: 'ExampleHT18', account_id: 14, workflow_state: 'available', course_code: 'H', is_public: true, name: 'Example course I HT18'},
+      {id: 3, sis_course_id: 'ExampleVT18', account_id: 14, workflow_state: 'available', course_code: 'B', is_public: true, name: 'Example course II VT18'},
+      {id: 3, sis_course_id: 'ExampleHT18', account_id: 24, workflow_state: 'available', course_code: 'G', is_public: true, name: 'Example course III HT18'},
+      {id: 3, sis_course_id: 'ExampleHT16', account_id: 24, workflow_state: 'available', course_code: 'E', is_public: true, name: 'Example course IV HT16'},
+      {id: 3, sis_course_id: 'ExampleVT17', account_id: 27, workflow_state: 'available', course_code: 'D', is_public: true, name: 'Example course V VT17'},
+      {id: 3, sis_course_id: 'ExampleHT17', account_id: 29, workflow_state: 'available', course_code: 'F', is_public: true, name: 'Example course VI HT18'},
+      {id: 3, sis_course_id: 'ExampleHT18', account_id: 63, workflow_state: 'available', course_code: 'A', is_public: true, name: 'Example course VII HT18'},
+      {id: 3, sis_course_id: 'ExampleHT18', account_id: 67, workflow_state: 'available', course_code: 'C', is_public: true, name: 'Example course VIII HT18'},
+    ]
   }
+
   try {
     const courses = (await canvasApi.listCourses())
       .filter(course => course.sis_course_id)
