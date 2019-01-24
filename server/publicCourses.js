bb2370d6e57f0b487f7494ba90ce72e5bc23c6bc
Carlos Saito <exacs@users.noreply.github.com>
2019-01-24 14:25:58 +0100

    Add example where there is a course with a "strange" SIS ID

--- server/publicCourses.js
+++ server/publicCourses.js
@@ -16,6 +16,7 @@ async function fetchCourses() {
       {id: 3, sis_course_id: 'ExampleVT18', account_id: 14, workflow_state: 'available', course_code: 'B', is_public: true, name: 'Example course II VT18'},
       {id: 3, sis_course_id: 'ExampleHT18', account_id: 24, workflow_state: 'available', course_code: 'G', is_public: true, name: 'Example course III HT18'},
       {id: 3, sis_course_id: 'ExampleHT16', account_id: 24, workflow_state: 'available', course_code: 'E', is_public: true, name: 'Example course IV HT16'},
+      {id: 3, sis_course_id: 'Blablablabl', account_id: 67, workflow_state: 'available', course_code: 'C', is_public: true, name: 'Example course nowhere'},
       {id: 3, sis_course_id: 'ExampleVT17', account_id: 27, workflow_state: 'available', course_code: 'D', is_public: true, name: 'Example course V VT17'},
       {id: 3, sis_course_id: 'ExampleHT17', account_id: 29, workflow_state: 'available', course_code: 'F', is_public: true, name: 'Example course VI HT18'},
       {id: 3, sis_course_id: 'ExampleHT18', account_id: 63, workflow_state: 'available', course_code: 'A', is_public: true, name: 'Example course VII HT18'},
